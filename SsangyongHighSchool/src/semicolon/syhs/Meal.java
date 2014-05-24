0d7961058be809494feda358060de64a7a5fed2c
aaa0956 <wlwogud02@naver.com>
2014-05-24 19:24:43 +0900

    MealActivity 최적화

--- SsangyongHighSchool/src/semicolon/syhs/Meal.java
+++ SsangyongHighSchool/src/semicolon/syhs/Meal.java
@@ -26,191 +26,176 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Meal extends Activity {
-   
-    int month, date;
+	
     final String URL="http://hes.cne.go.kr/sts_sci_md00_001.do?schulCode=N100002511&insttNm=%EC%B2%9C%EC%95%88%EC%8C%8D%EC%9A%A9%EA%B3%A0%EB%93%B1%ED%95%99%EA%B5%90&schulCrseScCode=4&schulKndScCode=04&schYm=2014.";
-
+    int month, date;
     Source source;
     Calendar cal;
     ArrayList<String> mealArr;
     TextView mealTV,dateTV;
-    String dates;
-   
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_meal);
-      
-      cal=Calendar.getInstance();
-      date = cal.get(Calendar.DAY_OF_MONTH);
-      month = cal.get(Calendar.MONTH)+1;
-      
-      dates = String.valueOf(date);
-      
-      dateTV = (TextView)findViewById(R.id.day);
-      dateTV.setText(month+"월 "+date+"일 식단표");
-      
-      mealTV=(TextView)findViewById(R.id.meal);
-      getData();
-      
-      findViewById(R.id.next).setOnClickListener(listener);
-      findViewById(R.id.back).setOnClickListener(listener);
-      findViewById(R.id.today).setOnClickListener(listener);         
-   }
-   
-   /**OnClickListener**/
-   View.OnClickListener listener=new View.OnClickListener() {
-      
-      @Override
-      public void onClick(View v) {
-         switch(v.getId()) {
-         case R.id.back:
-            cal.set(Calendar.DATE, date-1);
-            if(month!=cal.get(Calendar.MONTH)+1) {
-               month=cal.get(Calendar.MONTH)+1;
-               initialize();
-               getData();
-            }
-            date=cal.get(Calendar.DATE);
-             mealTV.setText(mealArr.get(date-1));
-            dateTV.setText(month+"월 "+date+"일 식단표");
-            break;
-         case R.id.today:
-            cal=Calendar.getInstance();
-            if(month!=cal.get(Calendar.MONTH)+1) {
-               month=cal.get(Calendar.MONTH)+1;
-               initialize();
-               getData();
-            }
-            date=cal.get(Calendar.DATE);
-            mealTV.setText(mealArr.get(date-1));
-            dateTV.setText(month+"월 "+date+"일 식단표");
-            break;
-         case R.id.next:
-            cal.set(Calendar.DATE, date+1);
-            if(month!=cal.get(Calendar.MONTH)+1) {
-               month=cal.get(Calendar.MONTH)+1;
-               initialize();
-               getData();
-            }
-            date=cal.get(Calendar.DATE);
-            mealTV.setText(mealArr.get(date-1));
-            dateTV.setText(month+"월 "+date+"일 식단표");
-            break;
-         }
-      }
-   };
-   
-   /**Call when month is Changed**/
-   private void initialize() {
-      source=null;
-      mealArr.clear();
-   }    
-   
-   /**getData**/
-   private void getData() {
-      try {
-         getDataFromFile();
-      } catch(FileNotFoundException e) {
-         if(isNetworkAvailable()) {
-            getDataFromServer();
-         } else {
-            Toast.makeText(this,"네트워크 상태를 확인하세요",Toast.LENGTH_SHORT).show();
-            finish();
-         }
-      }      
-   }
-   
-   private Boolean isNetworkAvailable(){
-      ConnectivityManager connect = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-      if (connect.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == State.CONNECTED
-            || connect.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == State.CONNECTED ) {
-          return true;                
-      } else {
-          return false;    
-      }
-    }
+    Toast toast;
 
-   @SuppressWarnings("unchecked")
-   private void getDataFromFile() throws FileNotFoundException {
-      try {
-         FileInputStream fis = this.openFileInput(cal.get(Calendar.YEAR)+"."+month);
-         ObjectInputStream ois = new ObjectInputStream(fis);
-         ArrayList<String> t = (ArrayList<String>)ois.readObject();
-         mealArr = (ArrayList<String>) t.clone();         
-         ois.close();
-         mealTV.setText(mealArr.get(date-1));
-      }
-       catch (FileNotFoundException e) {
-          throw e;
-      } catch(IOException e) {
-         e.printStackTrace();
-      }
-      catch (ClassNotFoundException e) {
-         e.printStackTrace();
-      }
-   }
-   
-   
-   private void getDataFromServer() {      
-       Thread thread=new Thread() {
-         @Override
-         public void run(){
-            try {
-               String urlMonth;
-               if(month<10) {
-                  urlMonth="0"+month;
-                } else {
-                   urlMonth=""+month;
-                }
-               source=new Source(new InputStreamReader(new URL(URL+urlMonth).openStream(),"UTF-8"));
-               
-             } catch(MalformedURLException e) {
-                e.printStackTrace();
-             } catch(IOException e) {
-                e.printStackTrace();
-             }
-         }
-      };
-      thread.start();
-      
-      try {
-         thread.join();
-      } catch (InterruptedException e) {
-         e.printStackTrace();
-      }
-      List<Element> tr=source.getAllElements(HTMLElementName.TABLE).get(0).getAllElements(HTMLElementName.TR);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_meal);
+       
+       cal=Calendar.getInstance();
+       dateTV = (TextView)findViewById(R.id.day);      
+       mealTV=(TextView)findViewById(R.id.meal);
        mealArr=new ArrayList<String>();
-       for(int i=0;i<tr.size();i++) {
-          List<Element> tdList=tr.get(i).getAllElements(HTMLElementName.TD);
-          for(int j=0;j<tdList.size();j++) {
-             String toAdd=tdList.get(j).toString().replaceAll("<tr>", "")
-                   .replaceAll("</tr>", "").replaceAll("<td>", "").replaceAll("</td>", "")
-             .replaceAll("<br />", "\n").replace("[석식]", "\n[석식]")
-             .trim();
-             
-             if(!toAdd.isEmpty()) {            	 
-                if(toAdd.length()<5) {
-                   toAdd="급식이 없습니다.";
-                }
-                int index=toAdd.indexOf("[");
-                if(index==-1) {
-                	index=0;
-                }
-                mealArr.add(toAdd.substring(index));
-             }
-          }
-       }
-      mealArr.add("");
-      mealTV.setText(mealArr.get(date-1));
+       findViewById(R.id.back).setOnClickListener(listener);
+       findViewById(R.id.today).setOnClickListener(listener); 
+       findViewById(R.id.next).setOnClickListener(listener);    
       
-      try {
-          FileOutputStream fos = openFileOutput(cal.get(Calendar.YEAR)+"."+month, Context.MODE_PRIVATE);
-          ObjectOutputStream oos = new ObjectOutputStream(fos);
-          oos.writeObject(mealArr);
-          oos.close();
-      } catch (IOException e) {
-         e.printStackTrace();
-       }
-   }
+       initialize();
+    }
+    
+    private void initialize() {
+	    source=null;
+	    mealArr.clear();
+		month=cal.get(Calendar.MONTH)+1;
+		date=cal.get(Calendar.DATE);
+		try {
+			getDataFromFile();
+		} catch(FileNotFoundException e) {
+			if(isNetworkAvailable()) {
+				getDataFromServer();
+			}
+		} finally {
+			setText();
+		}
+    }
+    
+    private void setText() {
+		if(mealArr.isEmpty()) {
+			dateTV.setText("");
+			mealTV.setText("");
+			if(toast==null) {
+				toast=Toast.makeText(this,"네트워크 상태를 확인하세요.",Toast.LENGTH_SHORT);
+			}
+			toast.show();
+			return;
+		}
+    	month=cal.get(Calendar.MONTH)+1;
+		date=cal.get(Calendar.DATE);
+    	dateTV.setText(month+"월 "+date+"일 식단표");
+		mealTV.setText(mealArr.get(date-1));
+    }
+    
+    View.OnClickListener listener=new View.OnClickListener() {
+    	@Override
+    	public void onClick(View v) {
+    		switch(v.getId()) {
+    		case R.id.back:
+    			cal.set(Calendar.DATE, date-1);
+    			if(month!=cal.get(Calendar.MONTH)+1) {               
+    				initialize();
+    			} else {
+    				setText();
+    			}
+    			break;
+    		case R.id.today:
+    			cal=Calendar.getInstance();
+    			if(month!=cal.get(Calendar.MONTH)+1) {
+    				initialize();
+    			} else {
+    				setText();
+    			}
+    			break;
+    		case R.id.next:
+    			cal.set(Calendar.DATE, date+1);
+    			if(month!=cal.get(Calendar.MONTH)+1) {
+    				initialize();
+    			} else {
+    				setText();
+    			}
+    			break;
+    		}
+    	}
+    };
+    
+    private boolean isNetworkAvailable() {
+    	ConnectivityManager connect = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+    	if (connect.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == State.CONNECTED
+    			|| connect.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == State.CONNECTED ) {
+    		return true;               
+    	} else {
+    		return false;
+    	}
+    }
+    
+	@SuppressWarnings("unchecked")
+    private void getDataFromFile() throws FileNotFoundException {
+    	try {
+    		FileInputStream fis = this.openFileInput(cal.get(Calendar.YEAR)+"."+month);
+    		ObjectInputStream ois = new ObjectInputStream(fis);
+    		ArrayList<String> t = (ArrayList<String>)ois.readObject();
+    		mealArr = (ArrayList<String>) t.clone();         
+    		ois.close();
+    	}
+    	catch (FileNotFoundException e) {
+    		throw e;
+    	} catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    	catch (ClassNotFoundException e) {
+    		e.printStackTrace();
+    	}
+    }
+    
+    private void getDataFromServer() {
+    	Thread thread=new Thread() {    		
+    		@Override
+    		public void run(){
+    			try {
+    				String urlMonth;
+    				if(month<10) {
+    					urlMonth="0"+month;
+    				} else {
+    					urlMonth=""+month;
+    				}
+    				source=new Source(new InputStreamReader(new URL(URL+urlMonth).openStream(),"UTF-8"));    				
+    			} catch(MalformedURLException e) {
+    				e.printStackTrace();
+    			} catch(IOException e) {
+    				e.printStackTrace();
+    			}
+    		}
+    	};
+    	thread.start();    	
+    	try {
+    		thread.join();
+    	} catch (InterruptedException e) {
+    		e.printStackTrace();
+    	}
+    	List<Element> tr=source.getAllElements(HTMLElementName.TABLE)
+    			.get(0).getAllElements(HTMLElementName.TR);
+    	for(int i=0;i<tr.size();i++) {
+    		List<Element> tdList=tr.get(i).getAllElements(HTMLElementName.TD);
+    		for(int j=0;j<tdList.size();j++) {
+    			String toAdd=tdList.get(j).toString().replaceAll("<tr>", "")
+    					.replaceAll("</tr>", "").replaceAll("<td>", "")
+    					.replaceAll("</td>", "").replaceAll("<br />", "\n")
+    					.replace("[석식]", "\n[석식]").trim();
+    			if(!toAdd.isEmpty()) {
+    				int index;
+    				if((index=toAdd.indexOf("["))==-1) {
+    					toAdd="급식이 없습니다.";
+    				} else {
+    					toAdd=toAdd.substring(index);
+    				}
+    				mealArr.add(toAdd);
+    			}
+    		}
+    	}
+    	try {
+    		FileOutputStream fos = openFileOutput(cal.get(Calendar.YEAR)+"."+month, Context.MODE_PRIVATE);
+    		ObjectOutputStream oos = new ObjectOutputStream(fos);
+    		oos.writeObject(mealArr);
+    		oos.close();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    }
 }
